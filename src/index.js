672126c433b9b625f06bf15fe33c680d8899cfee
Kyle Phillips <justinkphillips@gmail.com>
2022-03-06 14:07:02 +0100

    add styling for todos and temp code to add test set upon refresh

--- src/index.js
+++ src/index.js
@@ -7,11 +7,20 @@ const deadline = formInput.querySelector("#duedate")
 
 function addItem(todotext, ranking, dueDate) {
   const todoRank = document.querySelector(`.${ranking}`)
-  console.log(todoRank)
     const template = `
-    <div class="${ranking}" data-date="${new Date().toLocaleTimeString()}">
-    <p>${todotext}</p>
+    <div class="${ranking}item" data-date="${new Date().toLocaleTimeString()}">
+    <div class="dateelement">
+    <span>Due by:</span>
     <span>${dueDate}</span>
+    </div>
+    <div class="todoelement">
+    <span>Task Description:</span>
+    <p>${todotext}</p>
+    </div>
+    <div class="todobuttons">
+    <button id="complete" class="btn btn-success">Done!</button>
+    <button id="delete" class="btn btn-danger">Remove</button>
+    </div>
     </div>
     `;
     todoRank.innerHTML += template;
@@ -20,6 +29,15 @@ function addItem(todotext, ranking, dueDate) {
 function handleSubmitForm(event) {
   event.preventDefault();
   addItem(description.value, priority.value, deadline.value)
+  formInput.reset();
 }
 
   formInput.addEventListener("submit", handleSubmitForm);
+
+  function testToDos() {
+    addItem("Add urgent ToDo", "urgent", "2022-03-06" );
+    addItem("Add important ToDo", "important", "2022-03-06" );
+    addItem("Add nice-to-have ToDo", "nicetohave", "2022-03-06" );
+  }
+
+  testToDos()
