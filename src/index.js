2aa87a0fefd75cd755c5f0fcabc0841896908310
Eike mx <merxeike@gmail.com>
2022-03-12 12:57:28 +0000

    implemented local storrage

--- src/index.js
+++ src/index.js
@@ -3,25 +3,33 @@ const description = formInput.todotext;
 const priority = formInput.ranking;
 const deadline = formInput.querySelector("#duedate")
 
+const APP_NAMESPACE = 'DoItNow';
 
+const rankings = ["urgent", 'important', 'nicetohave']
+
+let tasks = []
+
+if (localStorage.getItem(APP_NAMESPACE)){
+  tasks = JSON.parse(localStorage.getItem(APP_NAMESPACE));
+}
 
 function addItem(todotext, ranking, dueDate) {
   const todoRank = document.querySelector(`.${ranking}`)
     const template = `
     <div class="${ranking}item" data-date="${new Date().toLocaleTimeString()}">
-    <div class="dateelement">
-    <span>Due by:</span>
-    <span>${dueDate}</span>
-    <span id="status" class="status">Pending</span>
-    </div>
-    <div class="todoelement">
-    <span>Task Description:</span>
-    <p>${todotext}</p>
-    </div>
-    <div class="todobuttons">
-    <button id="complete" class="btn btn-success">Done!</button>
-    <button id="remove" class="btn btn-danger">Remove</button>
-    </div>
+      <div class="dateelement">
+        <span>Due by:</span>
+        <span>${dueDate}</span>
+        <span id="status" class="status">Pending</span>
+        </div>
+        <div class="todoelement">
+        <span>Task Description:</span>
+        <p>${todotext}</p>
+        </div>
+        <div class="todobuttons">
+        <button id="complete" class="btn btn-success">Done!</button>
+        <button id="remove" class="btn btn-danger">Remove</button>
+      </div>
     </div>
     `;
     todoRank.innerHTML += template;
@@ -29,76 +37,92 @@ function addItem(todotext, ranking, dueDate) {
 
 function handleSubmitForm(event) {
   event.preventDefault();
-  addItem(description.value, priority.value, deadline.value)
+  tasks.push({
+    name: description.value,
+    ranking:  priority.value,
+    dueDate: deadline.value
+  })
+  localStorage.setItem(APP_NAMESPACE, JSON.stringify(tasks));
+  cleanAndRender();
   formInput.reset();
 }
 
-  formInput.addEventListener("submit", handleSubmitForm);
+formInput.addEventListener("submit", handleSubmitForm);
 
-  function testToDos() {
-    addItem("Add urgent ToDo", "urgent", "2022-03-06" );
-    //addItem("Add important ToDo", "important", "2022-03-06" );
-    addItem("Add nice-to-have ToDo", "nicetohave", "2022-03-06" );
+function clearElement(ranking) {
+  const element = document.querySelector(`.${ranking}`)
+  while (element.firstChild) {
+    element.removeChild(element.firstChild)
+  }
+}
+
+function cleanAndRender() {
+  rankings.forEach(ranking => clearElement(ranking) )
+  renderTodos()
+}
+
+  function renderTodos() {
+    tasks.forEach(task => addItem(task.name, task.ranking, task.dueDate))
   }
 
-  testToDos();
+  renderTodos();
 
   // Cheerful message when the section is empty
 
-  const messageArray = [["Nothing on the agenda for today. Time for a coffee!"],
-                         ["There's nothing to see here. Get back to your knitting."],
-                         ["Not all who wander are lost. But these tasks are..."],
-                         ["What will you do with all of this free time?"],
-                         ["Done and dusted. I do believe it is time for tea."],
-                         ["This day has been a success. You should treat yourself to a bath."],
-                         ["You should be proud of the fact that you have nothing to do."],
-                         ["Time to catch up on your bedside reading."],
-                         ["With all your tasks out of the way there is plenty time for snacks!"]];
+  // const messageArray = [["Nothing on the agenda for today. Time for a coffee!"],
+  //                        ["There's nothing to see here. Get back to your knitting."],
+  //                        ["Not all who wander are lost. But these tasks are..."],
+  //                        ["What will you do with all of this free time?"],
+  //                        ["Done and dusted. I do believe it is time for tea."],
+  //                        ["This day has been a success. You should treat yourself to a bath."],
+  //                        ["You should be proud of the fact that you have nothing to do."],
+  //                        ["Time to catch up on your bedside reading."],
+  //                        ["With all your tasks out of the way there is plenty time for snacks!"]];
 
-  const randomCheerGenerator = () => 
-    {
-        const cheerfulMessage = Math.floor(Math.random() * 8);
-        const location1 = cheerfulMessage > 8 ? cheerfulMessage - 1 : cheerfulMessage;
-        const location2 = cheerfulMessage > 7 ? cheerfulMessage - 2 : cheerfulMessage + 1;
-        const location3 = cheerfulMessage > 6 ? cheerfulMessage - 3 : cheerfulMessage + 2;
-        const message1 = messageArray[location1];
-        const message2 = messageArray[location2];
-        const message3 = messageArray[location3];
-        addRandomMessages(message1, message2, message3);
-    };
+  // const randomCheerGenerator = () => 
+  //   {
+  //       const cheerfulMessage = Math.floor(Math.random() * 8);
+  //       const location1 = cheerfulMessage > 8 ? cheerfulMessage - 1 : cheerfulMessage;
+  //       const location2 = cheerfulMessage > 7 ? cheerfulMessage - 2 : cheerfulMessage + 1;
+  //       const location3 = cheerfulMessage > 6 ? cheerfulMessage - 3 : cheerfulMessage + 2;
+  //       const message1 = messageArray[location1];
+  //       const message2 = messageArray[location2];
+  //       const message3 = messageArray[location3];
+  //       addRandomMessages(message1, message2, message3);
+  //   };
 
-    function addRandomMessages(message1, message2, message3) {
-      const messageLocation1 = document.querySelector(".urgent");
-      const messageLocation2 = document.querySelector(".important");
-      const messageLocation3 = document.querySelector(".nicetohave");
-        const messageTemplate1 = `
-        <div class="randomMessage">
-        <span>${message1}</span>
-        </div>
-        `;
-        const messageTemplate2 = `
-        <div class="randomMessage">
-        <span>${message2}</span>
-        </div>
-        `;
-        const messageTemplate3 = `
-        <div class="randomMessage">
-        <span>${message3}</span>
-        </div>
-        `;
+  //   function addRandomMessages(message1, message2, message3) {
+  //     const messageLocation1 = document.querySelector(".urgent");
+  //     const messageLocation2 = document.querySelector(".important");
+  //     const messageLocation3 = document.querySelector(".nicetohave");
+  //       const messageTemplate1 = `
+  //       <div class="randomMessage">
+  //       <span>${message1}</span>
+  //       </div>
+  //       `;
+  //       const messageTemplate2 = `
+  //       <div class="randomMessage">
+  //       <span>${message2}</span>
+  //       </div>
+  //       `;
+  //       const messageTemplate3 = `
+  //       <div class="randomMessage">
+  //       <span>${message3}</span>
+  //       </div>
+  //       `;
 
-        if (document.querySelector(".urgentitem").length === 0) {
-          messageLocation1.innerHTML += messageTemplate1;
-        };
-        if (document.querySelector(".importantitem").length === 0) {
-          messageLocation2.innerHTML += messageTemplate2;
-        };
-        if (document.querySelector(".nicetohaveitem").length === 0) {
-          messageLocation3.innerHTML += messageTemplate3;
-        }
-    }
+  //       if (document.querySelector(".urgentitem").length === 0) {
+  //         messageLocation1.innerHTML += messageTemplate1;
+  //       };
+  //       if (document.querySelector(".importantitem").length === 0) {
+  //         messageLocation2.innerHTML += messageTemplate2;
+  //       };
+  //       if (document.querySelector(".nicetohaveitem").length === 0) {
+  //         messageLocation3.innerHTML += messageTemplate3;
+  //       }
+  //   }
 
-    randomCheerGenerator();
+  //   randomCheerGenerator();
     
     // Delete and Done button functions
     
@@ -110,7 +134,7 @@ function handleSubmitForm(event) {
   }
 
   function deleteTask(event) {
-     const deleteLocale = event.currentTarget.getParent
+     const deleteLocale = event.currentTarget.getParent // getParent ⚠️
   console.log(deleteLocale);
   }
 
@@ -123,7 +147,7 @@ function activateCompleteButtons() {
     
 function completeTask(event) {
   const completeLocale = event.currentTarget.parentNode.parentNode;
-  const status = completeLocale.getChild;
+  const status = completeLocale.getChild;  // getChild ⚠️
   console.log(completeLocale);
   //status.className = "statuscomplete";
   //status.innerText = "Complete";
